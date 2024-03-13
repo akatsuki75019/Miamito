ed7b69ceec4072eb755edece6b927e7fb827228a
AxelCourtois <courtois.a@hotmail.com>
2024-03-13 19:41:37 +0100

    Test Axel (login page)

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,14 @@
-import LoginForm from '../components/LoginForm';
+import LoginForm from "../components/LoginForm";
 function Login() {
-
-  return (
-    <>
-      <h1>Login</h1>
-      <LoginForm />
-      <a href="/register">Not registered yet ?</a>
-      {" | "}
-      <a href="/password/reset">Forgot password ?</a>
-    </>
-  );
+	return (
+		<>
+			<h1>Login</h1>
+			<LoginForm />
+			<a href="/register">Not registered yet YO ?</a>
+			{" | "}
+			<a href="/password/reset">Forgot password ?</a>
+		</>
+	);
 }
 
 export default Login;
