db3db040091efb0e7c9e86e503c6c20ceb4653a5
Samalzdh <sam.alzdh@gmail.com>
2024-03-14 12:09:22 +0100

    cleaning store

--- client/src/stores/store.js
+++ client/src/stores/store.js
@@ -1,30 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 
-// const rootReducer = combineReducers({
-//   auth: authReducer,
-//   // Ajoutez d'autres reducers ici
-// });
-
-// const persistConfig = {
-//   key: "root",
-//   version: 1,
-//   storage,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// export const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
-// export const persistor = persistStore(store);
-
 export default configureStore({
   reducer: {
     auth: authReducer,
