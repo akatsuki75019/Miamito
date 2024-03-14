ba81882e368ed722bedc2cc58ba160cec52a3b0b
AxelCourtois <courtois.a@hotmail.com>
2024-03-14 17:55:47 +0100

    Formulaire d'inscription avec Shadcn et zod

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -6,73 +6,74 @@ import { loginSuccess } from "../features/authSlice";
 import { LoginFetch } from "../services/authService";
 
 export default function LoginFrom() {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-  const dispatch = useDispatch();
-  const test = useSelector((state) => state.auth);
-  console.log(test.token);
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+	});
+	const dispatch = useDispatch();
+	const test = useSelector((state) => state.auth);
+	console.log(test.token);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const { token, data } = await LoginFetch(
-        formData.email,
-        formData.password
-      );
-      if (token) {
-        dispatch(loginSuccess(token)); // Dispatcher l'action avec le token
-        localStorage.setItem("token", token);
-        Cookies.set("token", token);
-        console.log(token);
-        window.location.href = "/";
-        console.log("You are logged in.", data.message);
-      } else {
-        console.error("Token is missing from the response.");
-      }
-    } catch (error) {
-      console.error("Failed to login:", error.message);
-    }
-  };
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormData((prevState) => ({
+			...prevState,
+			[name]: value,
+		}));
+	};
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Enter email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          autoComplete="email"
-        />
-        <Form.Text className="text-muted">
-          We will never share your email with anyone else!
-        </Form.Text>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          autoComplete="current-password"
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  );
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const { token, data } = await LoginFetch(
+				formData.email,
+				formData.password
+			);
+			if (token) {
+				dispatch(loginSuccess(token)); // Dispatcher l'action avec le token
+				localStorage.setItem("token", token);
+				Cookies.set("token", token);
+				console.log(token);
+				window.location.href = "/";
+				console.log("You are logged in.", data.message);
+			} else {
+				console.error("Token is missing from the response.");
+			}
+		} catch (error) {
+			console.error("Failed to login:", error.message);
+		}
+	};
+
+	return (
+		<Form onSubmit={handleSubmit}>
+			<Form.Group className="mb-3" controlId="formBasicEmail">
+				<Form.Label>Email address</Form.Label>
+				<Form.Control
+					type="email"
+					placeholder="Enter email"
+					name="email"
+					value={formData.email}
+					onChange={handleChange}
+					autoComplete="email"
+				/>
+				<Form.Text className="text-muted">
+					We will never share your email with anyone else!
+				</Form.Text>
+			</Form.Group>
+			<Form.Group className="mb-3" controlId="formBasicPassword">
+				<Form.Label>Password</Form.Label>
+				<Form.Control
+					type="password"
+					placeholder="Password"
+					name="password"
+					value={formData.password}
+					onChange={handleChange}
+					autoComplete="current-password"
+				/>
+			</Form.Group>
+			<Button variant="primary" type="submit">
+				Submit
+			</Button>
+		</Form>
+	);
 }
--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -1,88 +1,132 @@
 import { useState } from "react";
-
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { RegisterFetch } from "../services/authService";
+import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
+
+const registerFormSchema = z.object({
+	email: z.string().email(),
+	password: z.string().min(8),
+	confirmPassword: z.string().min(8),
+});
+
+// function RegisterForm() {
+// 	const [formData, setFormData] = useState({
+// 		user: {
+// 			email: "",
+// 			password: "",
+// 			confirmPassword: "",
+// 		},
+// 	});
+
+// 	const handleChange = (e) => {
+// 		const { name, value } = e.target;
+// 		setFormData((prevState) => ({
+// 			...prevState,
+// 			user: {
+// 				...prevState.user,
+// 				[name]: value,
+// 			},
+// 		}));
+// 	};
+
+// 	const handleSubmit = async (e) => {
+// 		e.preventDefault();
+// 		if (formData.user.password !== formData.user.confirmPassword) {
+// 			alert("Passwords do not match");
+// 			return;
+// 		}
+// 		try {
+// 			const data = await RegisterFetch(
+// 				formData.user.email,
+// 				formData.user.password
+// 			);
+// 			console.log(data);
+// 			window.location.href = "/";
+// 		} catch (error) {
+// 			alert("Failed to register: " + error.message);
+// 		}
+// 	};
 
 function RegisterForm() {
-  const [formData, setFormData] = useState({
-    user: {
-      email: "",
-      password: "",
-      confirmPassword: "",
-    },
-  });
+	const form =
+		useForm <
+		z.infer <
+		typeof registerFormSchema >>
+			{
+				resolver: zodResolver(registerFormSchema),
+				defaultValues: {
+					email: "",
+					password: "",
+					confirmPassword: "",
+				},
+			};
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      user: {
-        ...prevState.user,
-        [name]: value,
-      },
-    }));
-  };
+	const handleSubmit = () => {
+		console.log("submitted");
+	};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (formData.user.password !== formData.user.confirmPassword) {
-      alert("Passwords do not match");
-      return;
-    }
-    try {
-      const data = await RegisterFetch(
-        formData.user.email,
-        formData.user.password
-      );
-      console.log(data);
-      window.location.href = "/";
-    } catch (error) {
-      alert("Failed to register: " + error.message);
-    }
-  };
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(handleSubmit)}>
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }) => {
+							return (
+								<FormItem>
+									<FormMessage />
+								</FormItem>
+							);
+						}}
+					></FormField>
+				</form>
+			</Form>
+		</main>
+		// <Form onSubmit={handleSubmit}>
+		// 	<Form.Group className="mb-3" controlId="formBasicEmail">
+		// 		<Form.Label>Email address</Form.Label>
+		// 		<Form.Control
+		// 			type="email"
+		// 			name="email"
+		// 			value={formData.user.email}
+		// 			onChange={handleChange}
+		// 			placeholder="Enter email"
+		// 		/>
+		// 		<Form.Text className="text-muted">
+		// 			We will never share your email with anyone else.
+		// 		</Form.Text>
+		// 	</Form.Group>
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type="email"
-          name="email"
-          value={formData.user.email}
-          onChange={handleChange}
-          placeholder="Enter email"
-        />
-        <Form.Text className="text-muted">
-          We will never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+		// 	<Form.Group className="mb-3" controlId="formBasicPassword">
+		// 		<Form.Label>Password</Form.Label>
+		// 		<Form.Control
+		// 			type="password"
+		// 			name="password"
+		// 			value={formData.user.password}
+		// 			onChange={handleChange}
+		// 			placeholder="Password"
+		// 		/>
+		// 	</Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          name="password"
-          value={formData.user.password}
-          onChange={handleChange}
-          placeholder="Password"
-        />
-      </Form.Group>
+		// 	<Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+		// 		<Form.Label>Confirm Password</Form.Label>
+		// 		<Form.Control
+		// 			type="password"
+		// 			name="confirmPassword"
+		// 			value={formData.user.confirmPassword}
+		// 			onChange={handleChange}
+		// 			placeholder="Confirm Password"
+		// 		/>
+		// 	</Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
-        <Form.Label>Confirm Password</Form.Label>
-        <Form.Control
-          type="password"
-          name="confirmPassword"
-          value={formData.user.confirmPassword}
-          onChange={handleChange}
-          placeholder="Confirm Password"
-        />
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  );
+		// 	<Button variant="primary" type="submit">
+		// 		Submit
+		// 	</Button>
+		// </Form>
+	);
 }
 
 export default RegisterForm;
