6adfa1a6263cf9550a58de3132757ab4dea98cff
AxelCourtois <courtois.a@hotmail.com>
2024-03-15 18:24:10 +0100

    Ajout SideBar

--- client/src/components/AppRoutes.jsx
+++ client/src/components/AppRoutes.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import ResetPassword from "../pages/ResetPassword";
+
 function AppRoutes() {
 	return (
 		<Routes>
