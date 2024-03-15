811f59c7df8bba29688bf12387977424cd3264de
AxelCourtois <courtois.a@hotmail.com>
2024-03-15 23:07:33 +0100

    Navbar encore et toujours

--- client/src/components/nav/NavBar.jsx
+++ client/src/components/nav/NavBar.jsx
@@ -10,46 +10,45 @@ import { importImage } from "../../features/importImage";
 import { ModeToggle } from "../theme/mode-toggle";
 
 function NavBar() {
-  const isSmallDisplay = useIsSmallDisplay();
-  const images = importImage();
-  console.log(images.logo);
+	const isSmallDisplay = useIsSmallDisplay();
+	const images = importImage();
 
-  return (
-    <>
-      <section
-        className={`flex flex-row bg-slate-50  mb-16 ${
-          isSmallDisplay ? "justify-between" : "justify-around"
-        }`}
-      >
-        <div>
-          <RouterLink to="/">
-            <img src={images.logo} alt="Logo Miamito" className="w-28 h-auto" />
-          </RouterLink>
-        </div>
-        <div className="hidden sm:flex items-center">
-          <Button
-            variant="link"
-            className="font-medium text-base p-0 my-0 mx-3"
-          >
-            <RouterLink to="/">Home</RouterLink>
-          </Button>
-          <RecipeDropdown />
-          <BlogDropdown />
-          <AboutUsDropdown />
-        </div>
-        <div className="flex items-center">
-          {isSmallDisplay ? (
-            <SideBar />
-          ) : (
-            <>
-              {" "}
-              <ModeToggle /> <AccountDropdown />
-            </>
-          )}
-        </div>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<section
+				className={`flex flex-row mt-6 mb-16 ${
+					isSmallDisplay ? "justify-between" : "justify-around"
+				}`}
+			>
+				<div>
+					<RouterLink to="/">
+						<img src={images.logo} alt="Logo Miamito" className="w-28 h-auto" />
+					</RouterLink>
+				</div>
+				<div className="hidden sm:flex items-center pb-5">
+					<Button
+						variant="link"
+						className="font-medium text-base p-0 my-0 mx-3"
+					>
+						<RouterLink to="/">Home</RouterLink>
+					</Button>
+					<RecipeDropdown />
+					<BlogDropdown />
+					<AboutUsDropdown />
+				</div>
+				<div className="flex items-center pb-5">
+					{isSmallDisplay ? (
+						<SideBar />
+					) : (
+						<>
+							{" "}
+							<ModeToggle /> <AccountDropdown />
+						</>
+					)}
+				</div>
+			</section>
+		</>
+	);
 }
 
 export default NavBar;
