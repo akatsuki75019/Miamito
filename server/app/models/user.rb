b2ca0513f3c93aa53b52a6dc7135002541b4c303
AxelCourtois <courtois.a@hotmail.com>
2024-03-19 08:52:55 +0100

    Changement Seed + cards articles du blog

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :validatable, :jwt_authenticatable,
         jwt_revocation_strategy: JwtDenylist
   validates :email, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
   has_many :articles
   
   def full_name
