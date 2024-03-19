add6b1274e4e93dc8ec397bd7fdeb3c3db0af31e
AxelCourtois <courtois.a@hotmail.com>
2024-03-19 07:53:04 +0100

    Bug lors de l'import d'un composant

--- client/src/components/articles/ArticlesIndex.jsx
+++ client/src/components/articles/ArticlesIndex.jsx
@@ -2,32 +2,48 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { API_URL } from "../../constants";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "../ui/card";
 
 const ArticlesIndex = () => {
-  const [articles, setArticles] = useState([]);
+	const [articles, setArticles] = useState([]);
 
-  useEffect(() => {
-    const fetchArticles = async () => {
-      const response = await axios.get(`${API_URL}/articles`);
-      setArticles(response.data);
-    };
+	useEffect(() => {
+		const fetchArticles = async () => {
+			const response = await axios.get(`${API_URL}/articles`);
+			setArticles(response.data);
+		};
 
-    fetchArticles();
-  }, []);
+		fetchArticles();
+	}, []);
 
-  return (
-    <div>
-      <h1>Articles</h1>
-      {articles.map((article) => (
-        <div key={article.id}>
-          <h2>
-            <Link to={`/articles/${article.id}`}>{article.title}</Link>
-          </h2>
-          <p>{article.content.substring(0, 100)}...</p>
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<>
+			<h1>Blog</h1>
+			<div className="grid grid-cols-2 gap-4">
+				{articles.map((article) => (
+					<div key={article.id}>
+						<Link to={`/articles/${article.id}`}>
+							<Card>
+								<CardHeader>
+									<CardTitle>{article.title}</CardTitle>
+								</CardHeader>
+								<CardContent>
+									<p>{article.content.substring(0, 100)}...</p>
+								</CardContent>
+							</Card>
+						</Link>
+					</div>
+				))}
+			</div>
+		</>
+	);
 };
 
 export default ArticlesIndex;
