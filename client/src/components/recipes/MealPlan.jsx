18b28fcddedf5f1353606d1e4ddd14a6fbe9755f
Samalzdh <sam.alzdh@gmail.com>
2024-03-22 14:47:10 +0100

    correction fetch meals

--- client/src/components/recipes/MealPlan.jsx
+++ client/src/components/recipes/MealPlan.jsx
@@ -1,5 +1,5 @@
-import { fetchInformation, fetchMeals } from "@/features/recipesSlice";
-import { useEffect, useState } from "react";
+import { fetchMeals } from "@/features/recipesSlice";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import MealCard from "./MealCard";
 
@@ -13,7 +13,7 @@ export default function MealPlan() {
     if (localMeals.length === 0) {
       dispatch(fetchMeals());
     }
-  }, [localMeals, dispatch]);
+  }, [weekMeals.length, dispatch]);
 
   useEffect(() => {
     if (weekMeals.length > 0) {
@@ -21,28 +21,28 @@ export default function MealPlan() {
     }
   }, [weekMeals]);
 
-  useEffect(() => {
-    if (localMeals.length > 0) {
-      // Dispatch fetchInformation pour chaque repas
-      localMeals.forEach((meal) => {
-        let info = localStorage.getItem(`recipeInfo-${meal.id}`);
-        if (info) {
-          // Si les informations sont déjà dans le localStorage, mettez à jour l'état avec ces informations
-          setMealInfos((prevInfos) => [...prevInfos, JSON.parse(info)]);
-        } else {
-          // Sinon, dispatch fetchInformation et stockez le résultat dans le localStorage
-          dispatch(fetchInformation(meal.id)).then((newInfo) => {
-            localStorage.setItem(
-              `recipeInfo-${meal.id}`,
-              JSON.stringify(newInfo)
-            );
-            setMealInfos((prevInfos) => [...prevInfos, newInfo]);
-          });
-        }
-        console.log(mealInfos);
-      });
-    }
-  }, [localMeals, dispatch]);
+  // useEffect(() => {
+  //   if (localMeals.length > 0) {
+  //     // Dispatch fetchInformation pour chaque repas
+  //     localMeals.forEach((meal) => {
+  //       let info = localStorage.getItem(`recipeInfo-${meal.id}`);
+  //       if (info) {
+  //         // Si les informations sont déjà dans le localStorage, mettez à jour l'état avec ces informations
+  //         setMealInfos((prevInfos) => [...prevInfos, JSON.parse(info)]);
+  //       } else {
+  //         // Sinon, dispatch fetchInformation et stockez le résultat dans le localStorage
+  //         dispatch(fetchInformation(meal.id)).then((newInfo) => {
+  //           localStorage.setItem(
+  //             `recipeInfo-${meal.id}`,
+  //             JSON.stringify(newInfo)
+  //           );
+  //           setMealInfos((prevInfos) => [...prevInfos, newInfo]);
+  //         });
+  //       }
+  //       console.log(mealInfos);
+  //     });
+  //   }
+  // }, [localMeals, dispatch]);
 
   if (status === "loading") {
     return <div>Loading meals...</div>;
@@ -53,11 +53,11 @@ export default function MealPlan() {
     <div className="container">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         {localMeals &&
-          localMeals.length &&
+          localMeals.length > 0 &&
           localMeals.map((meal, index) => (
-            <>
-              <MealCard key={`mealCard-${meal.id}-${index}`} meal={meal} />
-            </>
+            <React.Fragment key={`mealCard-${meal.id}-${index}`}>
+              <MealCard meal={meal} />
+            </React.Fragment>
           ))}
       </div>
     </div>
