5191e24130ba5856b7d8cc545f89f6e127f8b670
Samalzdh <sam.alzdh@gmail.com>
2024-03-29 01:43:47 +0100

    main.jsx root render update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import { ThemeProvider } from "./components/theme/theme-provider.jsx";
 import "./index.css";
 import { persistor, store } from "./stores/store";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <ThemeProvider>
       <Provider store={store} persistor={persistor}>
