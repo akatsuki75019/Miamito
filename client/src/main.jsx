e0cb5aeb26a7e49024cf1c0baf4b38d04d637946
Samalzdh <sam.alzdh@gmail.com>
2024-03-29 03:13:59 +0100

    root update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.jsx";
 import { ThemeProvider } from "./components/theme/theme-provider.jsx";
 import "./index.css";
@@ -12,8 +13,10 @@ const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <Provider store={store} persistor={persistor}>
-        <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
