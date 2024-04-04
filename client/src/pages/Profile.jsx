5a572246f7f8ef252cdd0e534ba51e5c6ba6ac2a
AxelCourtois <courtois.a@hotmail.com>
2024-04-04 21:35:21 +0200

    Ajout profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,36 +9,47 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState, useEffect } from "react";
+import { ShowUser } from "@/services/userService";
 
 export default function Profile() {
-	const [email, setEmail] = useState(""); // Initialiser l'état de l'e-mail
+	const [user, setUser] = useState(""); // Initialiser l'état de l'e-mail
+	const [userFirstName, setUserFirstName] = useState(user.first_name);
+	const userName = user.first_name + " " + user.last_name;
 
 	useEffect(() => {
-		const fetchUserEmail = async () => {
-			const response = await fetch("/api/user"); // Remplacer par votre API
-			const user = await response.json();
-			setEmail(user.email);
+		const fetchUser = async () => {
+			const userData = await ShowUser();
+			setUser(userData);
 		};
 
-		fetchUserEmail();
+		fetchUser();
 	}, []);
+
 	return (
 		<Card className="w-full max-w-3xl mx-auto">
 			<CardHeader className="space-y-1">
-				<CardTitle>User profile</CardTitle>
+				<CardTitle>Welcome {userName}</CardTitle>
 				<CardDescription>You can edit your profile here</CardDescription>
 			</CardHeader>
 			<CardContent className="space-y-6">
 				<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
 					<div className="space-y-2">
 						<Label htmlFor="email">Email</Label>
-						<Input id="email" type="email" placeholder={email} />
+						<Input id="email" type="email" placeholder={user.email} />
 					</div>
 				</div>
-				<div className="mt-6">
-					<Button size="sm" variant="outline">
-						Change password
-					</Button>
+				<div className="flex flex-row mt-6 justify-between">
+					<div>
+						<Button size="sm" variant="outline">
+							Validate
+						</Button>
+					</div>
+					<div className="flex gap-3">
+						<Button size="sm" variant="outline">
+							Change password
+						</Button>
+						<Button size="sm">Delete account</Button>
+					</div>
 				</div>
 			</CardContent>
 		</Card>
